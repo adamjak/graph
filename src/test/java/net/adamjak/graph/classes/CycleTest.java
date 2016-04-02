e67314ed6bda1ab534620729d5af5b33f937a942
Tomas Adamjak <thomas@adamjak.net>
2016-04-02 11:41:32 +0200

    Create Unittest for develop cycle equals

---
+++ src/test/java/net/adamjak/graph/classes/CycleTest.java
@@ -0,0 +1,56 @@
+package net.adamjak.graph.classes;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Tomas Adamjak on 2.4.2016.
+ * Copyright 2016, Tomas Adamjak
+ * License: The BSD 3-Clause License
+ */
+public class CycleTest
+{
+
+	@Test
+	public void testAddVertexIntoCycle () throws Exception
+	{
+
+	}
+
+	@Test
+	public void testGetCycleElements () throws Exception
+	{
+
+	}
+
+	@Test
+	public void testGetCyrcleSize () throws Exception
+	{
+
+	}
+
+	@Test
+	public void testHasCommonVertex () throws Exception
+	{
+
+	}
+
+	@Test
+	public void testToString () throws Exception
+	{
+
+	}
+
+	@Test
+	public void testEquals () throws Exception
+	{
+
+	}
+
+	@Test
+	public void testCompareTo () throws Exception
+	{
+
+	}
+}
