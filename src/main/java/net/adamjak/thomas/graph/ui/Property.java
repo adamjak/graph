0332c0bd261684445d4e4cdfa3b17dce29b13e52
Tomas Adamjak <thomas@adamjak.net>
2016-11-08 21:50:30 +0100

    Add property file help text.

--- src/main/java/net/adamjak/thomas/graph/ui/Property.java
+++ src/main/java/net/adamjak/thomas/graph/ui/Property.java
@@ -16,7 +16,21 @@ public class Property
 	public static String getTextWithPropertyHelp()
 	{
 		StringBuilder sb = new StringBuilder();
-		// TODO: 1.11.2016 -- napisat help
+		sb.append("   Graph property file\n");
+        sb.append("-------------------------\n");
+        sb.append("Create with app:\n");
+        sb.append("\tYou can create with simple Java GUI application whitch you can find on https://github.com/adamjak/graph-property-file-creator\n");
+        sb.append("\n");
+        sb.append("Create in text editro:\n");
+        sb.append("\tyou can create property file with yourself in text editor like Vim or Notepad.\n");
+        sb.append("\tPropertu file have to have 3 lines with 3 properties:\n");
+        sb.append("\t 1. Line - graphAlgorithmPackage - You can create self graph algorithm implements my API and this propertie told app about package where classes are. If you haven't self graph algorithm value will be 'null'. For example: graphAlgorithmPackage=net.adamjak.thomas.graph\n");
+        sb.append("\t 2. Line folderWithGraphs - Full path to folder where you have graphs whitch you want test. For example: folderWithGraphs=/home/bob/graph-theory/library\n");
+        sb.append("\t 3. Line operation - Tell app what you want to do. You have 2 choices:\n");
+        sb.append("\t\t * BENCHMARK_ALL_ALGORITHMS - When you have to test graph with all algorithms and benchmark.\n");
+        sb.append("\t\t * ONLY_SNARK_TEST - When you have to only test if graph is snark.\n");
+        sb.append("\t Example for third line: operation=BENCHMARK_ALL_ALGORITHMS\n");
+
 		return sb.toString();
 	}
 }
