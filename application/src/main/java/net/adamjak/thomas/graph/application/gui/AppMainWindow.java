30417c98773f6276c3c58a1a072b43df546d3445
Tomas Adamjak <thomas@adamjak.net>
2016-11-23 18:51:04 +0100

    Add accelerators. Create load.

--- application/src/main/java/net/adamjak/thomas/graph/application/gui/AppMainWindow.java
+++ application/src/main/java/net/adamjak/thomas/graph/application/gui/AppMainWindow.java
@@ -1,10 +1,28 @@
 package net.adamjak.thomas.graph.application.gui;
 
+import net.adamjak.thomas.graph.library.api.Graph;
+import net.adamjak.thomas.graph.library.io.GraphFactory;
+
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by Tomas Adamjak on 22.11.2016.
@@ -24,32 +42,256 @@ public class AppMainWindow extends JFrame
 	private JPanel rootPanel;
 	private JScrollPane jScrollPane;
 	private JTable jTable;
+	private JMenuBar jMenuBar;
+
+	// --------------------------------------------------
+	// Other
+	// --------------------------------------------------
+	private AppMainWindow appMainWindow;
+	private List<Graph<Integer>> graphList = null;
 
 	public AppMainWindow ()
 	{
 		super(WINDOW_TITLE);
+		this.appMainWindow = this;
 
-		setIconImage(new ImageIcon(getClass().getClassLoader().getResource("logo.png")).getImage());
+		setIconImage(new ImageIcon(getClass().getClassLoader().getResource("logo-image/logo_64.png")).getImage());
 
 		setContentPane(rootPanel);
 		pack();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setJMenuBar(this.createJMenuBar());
 	}
 
 	private void createUIComponents ()
 	{
-		this.jTable = this.getTable();
+		this.jTable = this.createTable();
 	}
 
-	private JTable getTable ()
+	private JTable createTable ()
 	{
 		String[] columnNames = {"ID",
 				"Count of vertexes",
 				"Snark",
 				"Time",
 				"Algorithm name"};
+		String[][] data = this.getTableData();
 
-		Object[][] data = new Object[1][5];
-		return new JTable(data, columnNames);
+		JTable jTable = new JTable(data, columnNames);
+		DefaultTableModel model = new DefaultTableModel(data.length, columnNames.length);
+		jTable.setModel(model);
+		return jTable;
+	}
+
+	private String[][] getTableData ()
+	{
+		String[][] data;
+		if (this.graphList != null && this.graphList.size() > 0)
+		{
+			data = new String[this.graphList.size()][5];
+
+			for (int i = 0; i < this.graphList.size(); i++)
+			{
+				data[i][0] = String.valueOf(i);
+				data[i][1] = String.valueOf(this.graphList.get(i).getCountOfVertexes());
+				data[i][2] = "";
+				data[i][3] = "";
+				data[i][4] = "";
+			}
+		}
+		else
+		{
+			data = new String[0][5];
+		}
+
+		return data;
+	}
+
+	private void repaintTable ()
+	{
+		DefaultTableModel model = (DefaultTableModel) this.jTable.getModel();
+
+		while (model.getRowCount() > 0)
+		{
+			model.removeRow(0);
+		}
+
+		for (String[] s : this.getTableData())
+		{
+			model.addRow(s);
+		}
+	}
+
+	private JMenuBar createJMenuBar ()
+	{
+		this.jMenuBar = new JMenuBar();
+
+		// Menu File
+		JMenu jmFile = new JMenu("File");
+
+		// Menu File items
+		JMenuItem jmiFileOpen = new JMenuItem("Open");
+		jmiFileOpen.setAccelerator(GuiAccelerators.CTRL_O);
+		jmiFileOpen.addActionListener(new AlJmiFileOpen());
+		jmFile.add(jmiFileOpen);
+
+		// Submenu Save in File menu
+		JMenu jmFileSave = new JMenu("Save");
+
+		// Submenu Save items
+		JMenuItem jmiFileSaveResults = new JMenuItem("Save results");
+		jmFileSave.add(jmiFileSaveResults);
+		jmiFileSaveResults.setAccelerator(GuiAccelerators.CTRL_S);
+
+		JMenuItem jmiFileSaveSelected = new JMenuItem("Save selected items");
+		jmiFileSaveSelected.setAccelerator(GuiAccelerators.CTRL_SHIFT_S);
+		jmFileSave.add(jmiFileSaveSelected);
+
+		jmFile.add(jmFileSave);
+
+		jmFile.addSeparator();
+
+		JMenuItem jmiFileClose = new JMenuItem("Close");
+		jmiFileClose.setAccelerator(GuiAccelerators.CTRL_Q);
+		jmiFileClose.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed (ActionEvent e)
+			{
+				System.exit(0);
+			}
+		});
+		jmFile.add(jmiFileClose);
+
+		this.jMenuBar.add(jmFile);
+
+		// Menu selection
+		JMenu jmSelection = new JMenu("Selection");
+
+		// Menu Selection items
+		JMenuItem jmiSelectionSelectAll = new JMenuItem("Select all");
+		jmiSelectionSelectAll.setAccelerator(GuiAccelerators.CTRL_A);
+		jmSelection.add(jmiSelectionSelectAll);
+		JMenuItem jmiSelectionUnselectAll = new JMenuItem("Unselect all");
+		jmiSelectionUnselectAll.setAccelerator(GuiAccelerators.CTRL_SHIFT_A);
+		jmSelection.add(jmiSelectionUnselectAll);
+		JMenuItem jmiSelectionInvert = new JMenuItem("Invert");
+		jmiSelectionInvert.setAccelerator(GuiAccelerators.CTRL_I);
+		jmSelection.add(jmiSelectionInvert);
+
+		this.jMenuBar.add(jmSelection);
+
+		// Menu About
+		JMenu jmAbout = new JMenu("About");
+
+		// Menu about items
+		JMenuItem jmiAboutLicense = new JMenuItem("License");
+		jmiAboutLicense.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed (ActionEvent e)
+			{
+				String message = "<html>" +
+						"<h2>Copyright (c) 2016, Tomáš Adamják</h2>" +
+						"<p><strong>All rights reserved.</strong></p>" +
+						"<p>Redistribution and use in source and binary forms, with or without modification,<br />" +
+						"are permitted provided that the following conditions are met:</p>" +
+						"<ul>" +
+						"<li>Redistributions of source code must retain the above copyright notice,<br />" +
+						"this list of conditions and the following disclaimer.</li>" +
+						"<li>Redistributions in binary form must reproduce the above copyright notice,<br />" +
+						"this list of conditions and the following disclaimer in the documentation<br />" +
+						"and/or other materials provided with the distribution.</li>" +
+						"<li>Neither the name of the <organization> nor the names of its contributors may be<br />" +
+						"used to endorse or promote products derived from this software without specific prior written permission.</li>" +
+						"</ul>" +
+						"<p>THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND<br />" +
+						"ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED<br />" +
+						"WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE<br />" +
+						"DISCLAIMED. IN NO EVENT SHALL Tomáš Adamják BE LIABLE FOR ANY<br />" +
+						"DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES<br />" +
+						"(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;<br />" +
+						"LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND<br />" +
+						"ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT<br />" +
+						"(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS<br />" +
+						"SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.</p>";
+
+				JLabel formatedLabel = new JLabel(message);
+				formatedLabel.setFont(new Font("sans-serif", Font.PLAIN, 14));
+
+				JOptionPane.showMessageDialog(appMainWindow, formatedLabel, "The BSD 3-Clause License", JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
+		jmAbout.add(jmiAboutLicense);
+
+		JMenuItem jmiAboutAuthor = new JMenuItem("Author");
+		jmiAboutAuthor.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed (ActionEvent e)
+			{
+				Icon icon = new ImageIcon(getClass().getClassLoader().getResource("logo-image/logo_64.jpeg"));
+
+				String message = "<html>" +
+						"<h2>Tomáš Adamják</h2>" +
+						"<p><strong>E-mail:</strong> <code>thomas@adamjak.net</code></p>" +
+						"<p><strong>Web:</strong> <code>http://thomas.adamjak.net</code></p>" +
+						"<html>";
+
+				JLabel formatedLabel = new JLabel(message);
+				formatedLabel.setFont(new Font("sans-serif", Font.PLAIN, 14));
+				JOptionPane.showMessageDialog(appMainWindow, formatedLabel, "Author", JOptionPane.INFORMATION_MESSAGE, icon);
+			}
+		});
+		jmAbout.add(jmiAboutAuthor);
+
+		this.jMenuBar.add(jmAbout);
+
+		return jMenuBar;
+	}
+
+	private class AlJmiFileOpen implements ActionListener
+	{
+		@Override
+		public void actionPerformed (ActionEvent event)
+		{
+			JFileChooser jFileChooser = new JFileChooser();
+			jFileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+			jFileChooser.setMultiSelectionEnabled(true);
+
+			int status = jFileChooser.showOpenDialog(appMainWindow);
+			if (status == JFileChooser.APPROVE_OPTION)
+			{
+				List<Graph<Integer>> graphs = new LinkedList<Graph<Integer>>();
+
+				for (File f : jFileChooser.getSelectedFiles())
+				{
+					try
+					{
+						switch (GraphFactory.getFileFormat(f))
+						{
+							case GRAPH6:
+								graphs.addAll(GraphFactory.createGraphFromGraph6(f));
+								break;
+							case GRAPHML:
+								graphs.add(GraphFactory.createGraphFromGraphml(f));
+								break;
+							case BRATISLAVA_TEXT_CATALOG:
+							default:
+								graphs.addAll(GraphFactory.createGraphFromTextCatalog(f));
+								break;
+						}
+					}
+					catch (IOException e)
+					{
+						JOptionPane.showMessageDialog(appMainWindow, "File read error!\nFile:" + f.getName() + "\n" + e.getMessage(), "Error!", JOptionPane.ERROR_MESSAGE);
+						return;
+					}
+				}
+
+				graphList = graphs;
+				repaintTable();
+			}
+		}
 	}
 }
---
+++ application/src/main/java/net/adamjak/thomas/graph/application/gui/GuiAccelerators.java
@@ -0,0 +1,25 @@
+package net.adamjak.thomas.graph.application.gui;
+
+import javax.swing.KeyStroke;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+/**
+ * Created by Tomas Adamjak on 23.11.2016.
+ * Copyright 2016, Tomas Adamjak
+ * License: The BSD 3-Clause License
+ */
+public class GuiAccelerators
+{
+	public final static KeyStroke CTRL_A = KeyStroke.getKeyStroke(KeyEvent.VK_A, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask());
+	public final static KeyStroke CTRL_I = KeyStroke.getKeyStroke(KeyEvent.VK_I, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask());
+	public final static KeyStroke CTRL_O = KeyStroke.getKeyStroke(KeyEvent.VK_O, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask());
+	public final static KeyStroke CTRL_Q = KeyStroke.getKeyStroke(KeyEvent.VK_Q, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask());
+	public final static KeyStroke CTRL_S = KeyStroke.getKeyStroke(KeyEvent.VK_S, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask());
+
+	public final static KeyStroke CTRL_SHIFT_A = KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.CTRL_MASK + ActionEvent.SHIFT_MASK);
+	public final static KeyStroke CTRL_SHIFT_S = KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK + ActionEvent.SHIFT_MASK);
+
+
+}
