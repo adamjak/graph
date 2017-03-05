b277efe49426e70a535033ff40a7324bafa7bd03
Tomas Adamjak <thomas@adamjak.net>
2017-03-05 20:21:31 +0100

    mplement kowalik

---
+++ library/c_library/kowalik.h
@@ -0,0 +1,18 @@
+//
+// Created by Michal Povinsky on 04/08/16.
+//
+
+#ifndef KOWALIK_H
+#define KOWALIK_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int kowalik_colourise(char *grph, int grph_size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //KOWALIK_H
---
+++ library/src/main/java/net/adamjak/thomas/graph/library/tests/snarks/Kowalik.java
@@ -0,0 +1,10 @@
+package net.adamjak.thomas.graph.library.tests.snarks;
+
+/**
+ * Created by Tomas Adamjak on 27.2.2017.
+ * Copyright 2017, Tomas Adamjak
+ * License: The BSD 3-Clause License
+ */
+public class Kowalik
+{
+}
